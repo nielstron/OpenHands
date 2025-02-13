e28ce994a8b93b9b3eb02ffdcc49d9861df5e02a
Niels <niels.muendler@inf.ethz.ch>
2025-02-14 00:37:47 +0100

    Fix

--- extract_predictions.py
+++ extract_predictions.py
@@ -16,11 +16,17 @@ predictions = []
 with open(prediction_dir) as f:
     for line in f:
         pred = json.loads(line)
+        git_diff = pred["test_result"]["git_patch"]
+        try:
+            patchset = PatchSet(git_diff)
+        except:
+            _LOGGER.warning("No git diff found for instance %s", pred["instance_id"])
+            continue
         try:
             print(json.dumps({
                 "instance_id": pred["instance_id"],
                 "model_name_or_path": "OpenHands-Claude-Sonnet-3.5",
-                "model_patch": pred["test_result"]["git_patch"],
+                "model_patch": git_diff,
                 "full_output": json.dumps(pred),
             }))
         except KeyError:
